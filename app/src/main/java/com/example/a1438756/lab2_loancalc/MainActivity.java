bdfe339817e766c12dabb7ab9830e08912a199fa
Hannah Ly <lnh.hannah@gmail.com>
2017-09-07 15:59:26 -0400

    Committing for the first time.

---
+++ app/src/main/java/com/example/a1438756/lab2_loancalc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1438756.lab2_loancalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
